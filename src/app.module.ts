5ee7cfe1690c702b87766beb317f107a95f541bd
danieljesus <daniel.j.rodrigues06@gmail.com>
2020-11-12 14:12:41 -0300

    Create user module (module, controller, service, entity, repository)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { EventModule } from './modules/event/event.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { typeOrmConfig } from './config/typeorm.config'
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    EventModule
+    EventModule,
+    UserModule
   ],
 })
 export class AppModule {}
---
+++ src/modules/user/user.entity.ts
@@ -0,0 +1,17 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
+
+@Entity()
+@Unique(['email'])
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+  
+  @Column()
+  name: string
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+}
